caf88e09678448ba048a9057a53c0b24f48f34aa
thiagodornelles <thiago.azevedo87@gmail.com>
2024-11-13 02:04:04 -0300

    Loading our 32 bit kernel into memory and working with debugging symbols

---
+++ readme.md
@@ -0,0 +1,2 @@
+For MAC OS to work must install and set it to PATH variable
+https://formulae.brew.sh/formula/i686-elf-gcc:
--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -6,38 +6,37 @@ DATA_SEG equ gdt_data - gdt_start
 
 ;This routine is necessary to avoid code overwriting by BIOS BPB (BIOS Parameter Block)
 _start:
-  JMP SHORT start
-  NOP
+  jmp SHORT start
+  nop
 
 times 33 db 0
 
 start:
   ;Ensures that CS (code segment) is 0x7C0
-  JMP 0:step2
+  jmp 0:step2
 
 step2:
-  CLI ;Clear interrupts
+  cli ;Clear interrupts
   
   ;Setting up manually these segments to make sure its correctly set
-  MOV ax, 0 
-  MOV ds, ax ;Set data segment
-  MOV es, ax ;Set extra segment
+  mov ax, 0 
+  mov ds, ax ;Set data segment
+  mov es, ax ;Set extra segment
 
   ;Setting up stack segments
-  MOV ss, ax
-  MOV sp, 0x7C00
+  mov ss, ax
+  mov sp, 0x7C00
 
-  STI ;Enables interrupts
+  sti ;Enables interrupts
 
 load_protected:
-  CLI
-  LGDT[gdt_descriptor]
+  cli
+  lgdt[gdt_descriptor]
   ;Setting the last bit of cr0 (???)
-  MOV eax, cr0
-  OR eax, 0x1
-  MOV cr0, eax
-  JMP CODE_SEG:load32
-
+  mov eax, cr0
+  or eax, 0x1
+  mov cr0, eax
+  jmp CODE_SEG:load32
 
 ;GDT
 gdt_start:
@@ -71,15 +70,63 @@ gdt_descriptor:
 
 [BITS 32]
 load32:
-  MOV ax, DATA_SEG
-  MOV ds, ax
-  MOV es, ax
-  MOV fs, ax
-  MOV gs, ax
-  MOV ss, ax
-  MOV ebp, 0x00200000
-  MOV esp, ebp
-  JMP $
+  mov eax, 1
+  mov ecx, 100
+  mov edi, 0x0100000
+  call ata_lba_read
+  jmp CODE_SEG:0x0100000
+
+ata_lba_read:
+  mov ebx, eax ;Backup LBA
+  ;Send highest 8 bits of the lba to hard disk controller
+  shr eax, 24
+  or eax, 0xE0 ;select master drive
+  mov dx, 0x1f6
+  out dx, al
+  ;Finished sending highest 8 bits of the lba
+  ;Send the total sectors to read
+  mov eax, ecx
+  mov dx, 0x1f2
+  out dx, al
+  ;Finished total sectors to read
+  ;Send more bit to LBA
+  mov eax, ebx ; Restore LBA backup
+  mov dx, 0x1f3
+  out dx, al
+  ;Finished send more bits to LBA  
+  ;Send more bit to LBA
+  mov dx, 0x1f4
+  mov eax, ebx ; Restore LBA backup
+  shr eax, 8
+  out dx, al
+  ;Finished send more bits to LBA  
+  ;Send upper 16 bits to the LBA
+  mov dx, 0x1f5
+  mov eax, ebx ; Restore LBA backup
+  shr eax, 16
+  out dx, al
+  ;Finished send upper 16 bits to the LBA
+  mov dx, 0x1f7
+  mov al, 0x20
+  out dx, al
+
+;Read all sector into memory
+.next_sector:
+  push ecx
+;Checking if we need to read LBA again
+.try_again:
+  mov dx, 0x1f7
+  in al, dx
+  test al, 8
+  jz .try_again
+  ;We need to read 256 word at a time
+  mov ecx, 256
+  mov dx, 0x1f0
+  rep insw ;read from 0x100000 address(edi param) 256 words(ecx param)
+  pop ecx
+  loop .next_sector
+; End of reading sectors into memory
+  ret
 
 times 510-($ - $$) db 0
 dw 0xAA55
